9bee8ecc6b0cc104138088796d967d0de4e0b55e
Konstantin Borisenko <kostyaborisenko05@gmail.com>
2023-12-14 18:59:28 +0300

    Some small tweaks to the code

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,76 @@
-use image::{GenericImageView, ImageBuffer, Rgba};
+use std::fmt::Display;
 
-fn main() {
-    let filepath = "picture.png";
-    let xfactor: u32 = 4;
-    let yfactor: u32 = 4;
-    assert!(xfactor >= 1);
-    assert!(yfactor >= 1);
+use image::{GenericImageView, DynamicImage, ImageBuffer, Rgba};
 
-    let img = match image::open(filepath) {
-        Ok(img) => img,
-        Err(e) => {
-            println!("Something wrong happened: {}", e);
-            return;
+enum LogLevel {
+    Note,
+    Info,
+    Warning,
+    Error,
+}
+
+impl Display for LogLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LogLevel::Note    => write!(f, "- [NOTE]:"),
+            LogLevel::Info    => write!(f, "- [INFO]:"),
+            LogLevel::Warning => write!(f, "- [WARNING]:"),
+            LogLevel::Error   => write!(f, "- [ERROR]:"),
         }
-    };
+    }
+}
 
-    let (width, height): (u32, u32) = img.dimensions();
-    println!("[INFO]: Reading image with size: {}x{}.", width, height);
-    let (imgx, imgy): (u32, u32) = (width / xfactor, height / yfactor);
-    println!("[INFO]: Creating image with size: {}x{}.", imgx, imgy);
+const N: LogLevel = LogLevel::Note;
+const I: LogLevel = LogLevel::Info;
+const W: LogLevel = LogLevel::Warning;
+const E: LogLevel = LogLevel::Error;
 
-    let mut imgbuf = ImageBuffer::new(imgx, imgy);
+struct Image {
+    src: DynamicImage,
+    width: u32,
+    height: u32,
+}
 
-    println!("STATUS -> Generating image.");
-    for x in (0..width).step_by(xfactor as usize) {
-        if x + 1 == width || x / xfactor >= imgx {
-            break; // Some errors may happen
-        }
-        for y in (0..height).step_by(yfactor as usize) {
-            if y + 1 == height || y / yfactor >= imgy {
-                break; // Some errors may happen
+impl Image {
+    fn from_file(filepath: &str) -> Self {
+        println!("{N} Reading image form {}.", filepath);
+        let src = match image::open(filepath) {
+            Ok(img) => img,
+            Err(err) => {
+                panic!("{E} Wrong things happened:\n{}", err);
             }
+        };
 
+        let (width, height) = src.dimensions();
+        println!("{N} Reading image with size: {}x{}.", width, height);
+
+        Self {
+            src,
+            width,
+            height,
+        }
+    }
+}
+
+struct Buffer {
+    buf: ImageBuffer<Rgba<u8>, Vec<u8>>,
+    width: u32,
+    height: u32,
+}
+
+impl Buffer {
+    fn new(width: u32, height: u32) -> Self {
+        Self {
+            buf: ImageBuffer::new(width, height),
+            width,
+            height,
+        }
+    }
+}
+
+fn smol(img: Image, xfactor: u32, yfactor: u32, buffer: &mut Buffer) -> bool {
+    for x in 0..buffer.width {
+        for y in 0..buffer.height {
             let mut r: u16 = 0;
             let mut g: u16 = 0;
             let mut b: u16 = 0;
@@ -39,15 +78,15 @@ fn main() {
             let mut m: u16 = 0;
 
             for nx in 0..xfactor {
-                if x + nx >= width {
+                if x + nx >= img.width {
                     break;
                 }
                 for ny in 0..yfactor {
-                    if y + ny >= height {
+                    if y + ny >= img.height {
                         break;
                     }
 
-                    let p: Rgba<u8> = img.get_pixel(x + nx, y + ny);
+                    let p: Rgba<u8> = img.src.get_pixel(x * xfactor + nx, y * yfactor + ny);
 
                     r += p[0] as u16;
                     g += p[1] as u16;
@@ -63,12 +102,33 @@ fn main() {
             let a: u8 = (a / m) as u8;
 
             let pixel = Rgba([r, g, b, a]);
-            imgbuf.put_pixel(x / xfactor, y / yfactor, pixel);
+            buffer.buf.put_pixel(x, y, pixel);
         }
     }
-    println!("STATUS -> image is generated.");
+    return true;
+}
 
-    println!("STATUS -> writing image to file.");
-    imgbuf.save("smol_pic.png").unwrap();
-    println!("STATUS -> image is written.");
+fn main() {
+    let filepath = "picture.png";
+    let xfactor: u32 = 4;
+    let yfactor: u32 = 4;
+    assert!(xfactor >= 1);
+    assert!(yfactor >= 1);
+
+    let img: Image = Image::from_file(filepath);
+    let (imgx, imgy): (u32, u32) = (img.width / xfactor, img.height / yfactor);
+    let mut buf: Buffer = Buffer::new(imgx, imgy);
+
+    println!("{N} Creating image with size: {}x{}.", imgx, imgy);
+
+    println!("{I} STATUS -> Generating image.");
+    smol(img, xfactor, yfactor, &mut buf);
+    println!("{I} STATUS -> image is generated.");
+
+    println!("{I} STATUS -> writing image to file.");
+
+    println!("{W} We may fail here.");
+    buf.buf.save("smol_pic.png").unwrap();
+    println!("{I} STATUS -> image is written.");
+
 }
